b0d582ffa21f85c378445fa55e07f982b600a139
Steven Skoczen <steven.skoczen@wk.com>
2012-02-28 14:52:48 -0800

    only specifying logging info for non-django instances (for now)

--- heroku_web_autoscale/logger.py
+++ heroku_web_autoscale/logger.py
@@ -1,10 +1,15 @@
 import logging
 
 logger = logging.getLogger("heroku_web_autoscale")
-logger.setLevel(logging.WARN)
+
 
 ch = logging.StreamHandler()
-ch.setLevel(logging.WARN)
+
+try:
+    import django
+except:
+    logger.setLevel(logging.WARN)
+    ch.setLevel(logging.WARN)
 
 formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
