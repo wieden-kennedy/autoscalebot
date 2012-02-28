c0b185b4c84f8ca84e368407b07f6929f1418d69
Steven Skoczen <steven.skoczen@wk.com>
2012-02-28 15:41:20 -0800

    logging tweak

--- heroku_web_autoscale/logger.py
+++ heroku_web_autoscale/logger.py
@@ -5,11 +5,12 @@ logger = logging.getLogger("heroku_web_autoscale")
 
 ch = logging.StreamHandler()
 
-try:
-    import django
-except:
-    logger.setLevel(logging.WARN)
-    ch.setLevel(logging.WARN)
+# try:
+#     import django
+# except:
+#     logger.setLevel(logging.WARN)
+
+ch.setLevel(logging.WARN)
 
 formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
