44a244253e9f4fdc069bea5755ff6fae2aabd204
Steven Skoczen <steven.skoczen@wk.com>
2012-05-08 14:02:35 -0700

    added appdecision backend

--- heroku_web_autoscale/backends/measurement/appdecision.py
+++ heroku_web_autoscale/backends/measurement/appdecision.py
@@ -1,3 +1,5 @@
+import json
+import urllib2
 from heroku_web_autoscale import TOO_LOW, JUST_RIGHT, TOO_HIGH
 from heroku_web_autoscale.logger import logger
 
@@ -6,12 +8,42 @@ from heroku_web_autoscale.backends.measurement.base import BaseMeasurementBacken
 
 class AppDecisionBackend(BaseMeasurementBackend):
     """
+    This backend returns the json-encoded data from a measurement url response.
 
+    It accepts the following parameters:
+
+    measurement_url, which defaults to "/heroku-autoscale/measurement/", and
+    max_response_time_in_seconds, which defaults to 30.
+
+    It returns a dictionary, with the following format:
+
+    {
+        'backend': 'AppDecisionBackend',
+        'data': {
+            'custom-key-from-url": 'custom val from url',
+        },
+        'success': True,  // assuming it was
+    }
     """
-    def __init__(self, autoscaler, settings, min_time=500, max_time=1000, post_scale_wait_time=0, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(AppDecisionBackend, self).__init__(*args, **kwargs)
-        self.autoscaler = autoscaler
-        self.settings = settings
+        self.url = kwargs.get("measurement_url", "/heroku-autoscale/measurement/")
+        self.max_response_time_in_seconds = kwargs.get("max_response_time_in_seconds", 30)
 
-    def heartbeat(self):
-        raise NotImplemented("Not Implemented!!!")
+    def measure(self, *args, **kwargs):
+        success = True
+
+        try:
+            response = urllib2.urlopen(self.url, None, self.max_response_time_in_seconds)
+            json_resp = response.read()
+            assert json_resp is not None
+            app_dict = json.loads(json_resp)
+
+        except:  # probably URLError, but anything counts.
+            success = False
+
+        return {
+            'backend': 'AppDecisionBackend',
+            'data': app_dict,
+            'success': success
+        }
